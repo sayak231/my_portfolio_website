99036d017056dd53fbf255b593695fe7f02b0577
sayak231 <sayak649@gmail.com>
2020-09-01 23:27:39 +0530

    Updates

---
+++ precache-manifest.f7bb6c97c607b49cdcca2e0bdf0d447f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0249294bf3301efe9d68f6e614577e5",
+    "url": "/my_portfolio_website/index.html"
+  },
+  {
+    "revision": "1a532e6e87ea8b6fbdb3",
+    "url": "/my_portfolio_website/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "b8b366e4be21a2ce8edf",
+    "url": "/my_portfolio_website/static/css/main.9478c921.chunk.css"
+  },
+  {
+    "revision": "1a532e6e87ea8b6fbdb3",
+    "url": "/my_portfolio_website/static/js/2.69831620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_portfolio_website/static/js/2.69831620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b366e4be21a2ce8edf",
+    "url": "/my_portfolio_website/static/js/main.8dbdc2fb.chunk.js"
+  },
+  {
+    "revision": "568e36cff11f558d42b7",
+    "url": "/my_portfolio_website/static/js/runtime-main.643a3497.js"
+  },
+  {
+    "revision": "e1b3a883ed675b8508c7e0f39462f7a7",
+    "url": "/my_portfolio_website/static/media/aca-back.e1b3a883.jfif"
+  },
+  {
+    "revision": "3331e6a0c85dba99079deb24d1ccde66",
+    "url": "/my_portfolio_website/static/media/back.3331e6a0.jpg"
+  },
+  {
+    "revision": "c219eb0790813c0dc0a528729bf9e49f",
+    "url": "/my_portfolio_website/static/media/myAvatar.c219eb07.svg"
+  },
+  {
+    "revision": "9eb5af5ce054d5d07acdc2f2573a4ba3",
+    "url": "/my_portfolio_website/static/media/mypic.9eb5af5c.jpeg"
+  },
+  {
+    "revision": "900c35a7729348edef79331a5c3401d4",
+    "url": "/my_portfolio_website/static/media/sk-back.900c35a7.jpg"
+  },
+  {
+    "revision": "ae6fd3eb12d60fd0f42ae1f7cc21c61a",
+    "url": "/my_portfolio_website/static/media/world.ae6fd3eb.jpg"
+  }
+]);
